c05de4a07c4205f5ea3c55b1627b7ad94d1d859a
kudzi <kd.mutamba@hotmail.com>
2025-02-08 19:48:12 +0000

    Updating base route for gh pages

--- src/index.tsx
+++ src/index.tsx
@@ -28,7 +28,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="/church-website">
             <ThemeProvider theme={theme}>
                 <AppDataProvider>
                     <CssBaseline/>
